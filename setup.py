e8318837a5c30e87cfcd6e3fd9d6ecad927cd6af
hartytp <thomas.harty@physics.ox.ac.uk>
2019-04-18 14:51:17 +0100

    add setup script

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+
+console_scripts = [
+    "wand_server = wand.frontend.wand_server:main",
+    "wand_gui = wand.frontend.wand_gui:main",
+    "wand_locker = wand.frontend.wand_locker:main",
+    "wand_influx_db = wand.frontend.wand_influx_db:main",
+]
+
+setup(
+    name="wand",
+    version="0.1",
+    description="Wavelength Analysis 'Nd Display",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": console_scripts,
+    }
+)
