6988f9e4d3b386f149e9d9690e8867094be3ba25
David Nadlinger <code@klickverbot.at>
2021-04-05 19:52:46 +0100

    setup.py: Remove obsolete wand_locker frontend script

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ from setuptools import setup, find_packages
 console_scripts = [
     "wand_server = wand.frontend.wand_server:main",
     "wand_gui = wand.frontend.wand_gui:main",
-    "wand_locker = wand.frontend.wand_locker:main",
     "wand_influx_db = wand.frontend.wand_influx_db:main",
 ]
 
