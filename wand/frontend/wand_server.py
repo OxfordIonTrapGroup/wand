39de1dc51fa146cfd8738a8e45a399c178703aea
Jamie Leppard <42746584+JammyL@users.noreply.github.com>
2022-09-07 13:42:17 +0100

    Lock min/max pzt voltage can now be set in config (#49)
    * Lock min/max pzt voltage can be set in config
    
    Also improve logging for outside max v pzt range
    
    * Remove extra "'" in logging

--- wand/frontend/wand_server.py
+++ wand/frontend/wand_server.py
@@ -215,13 +215,15 @@ class WandServer:
                 set_point = conf["lock_set_point"]
                 gain = conf["lock_gain"] * poll_time
                 capture_range = conf["lock_capture_range"]
+                v_pzt_max = conf.get("v_pzt_max", 100)
+                v_pzt_min = conf.get("v_pzt_min", 25)
 
                 await asyncio.wait({self.wake_locks[laser].wait()},
                                    timeout=poll_time)
                 self.wake_locks[laser].clear()
 
                 if timeout is not None and time.time() > (locked_at + timeout):
-                    logger.info("'{}'' lock timed out".format(laser))
+                    logger.info("'{}' lock timed out".format(laser))
                     self.control_interface.unlock(laser, conf["lock_owner"])
                     await asyncio.sleep(0)
                     continue
@@ -237,7 +239,7 @@ class WandServer:
                 V_error = f_error * gain
 
                 if abs(f_error) > capture_range:
-                    logger.warning("'{}'' outside capture range".format(laser))
+                    logger.warning("'{}' outside capture range".format(laser))
                     self.control_interface.unlock(laser, conf["lock_owner"])
                     await asyncio.sleep(0)
                     continue
@@ -250,8 +252,16 @@ class WandServer:
                     v_pzt = iface.get_pzt_voltage()
                     v_pzt -= V_error
 
-                    if v_pzt > 100 or v_pzt < 25:
-                        logger.warning("'{}'' lock railed".format(laser))
+                    if v_pzt > v_pzt_max or v_pzt < v_pzt_min:
+                        logger.warning(
+                            "'{}' lock railed, piezo voltage: {:.2f}V outside range {} - {}V"
+                            .format(
+                                laser,
+                                v_pzt,
+                                v_pzt_min,
+                                v_pzt_max,
+                            )
+                        )
                         self.control_interface.unlock(laser,
                                                       conf["lock_owner"])
                         await asyncio.sleep(0)
