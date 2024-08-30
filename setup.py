a79904e0be328a44bc5a488b9b5d1d937f5014ce
David Nadlinger <code@klickverbot.at>
2024-08-30 15:55:01 +0100

    setup.py: Add setuptools dependency for pkg_resources
    This is no longer installed by default on Python 3.11 with Poetry.

--- setup.py
+++ setup.py
@@ -8,12 +8,19 @@ console_scripts = [
     "wand_influx_db = wand.frontend.wand_influx_db:main",
 ]
 
+requirements = [
+    # Explicitly depend on setuptools to bring back the deprecated pkg_resources
+    # before it is removed entirely (at which point we need a new solution).
+    "setuptools",
+]
+
 setup(
     name="wand",
     version="0.1",
     description="Wavelength Analysis 'Nd Display",
     packages=find_packages(),
     include_package_data=True,
+    install_requires=requirements,
     entry_points={
         "console_scripts": console_scripts,
     }
